5ca549fca90d4fc6801b7aba8d887623ca4859a0
Magdelena <magdelena.gavrilova@mail.com>
2022-01-21 16:22:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import './data.json'
+
+function App() {
+  return (
+    <div className="App">
+         <h1>Magdelena's Info Site probe </h1>
+       
+    </div>
+  );
+}
+
+export default App;
